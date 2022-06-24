0fdadf11da1766f1c8fa92d102e6e8e85f5c1349
Chuzume <chuzume@gmail.com>
2022-06-25 07:45:30 +0900

    Fix Script

--- script.lua
+++ script.lua
@@ -1,24 +1,119 @@
-
-
-local model_file_name = "model"
-models[model_file_name]:setPos(20,24,0)
-
 -- the "im too dumb to find them so I made my own" section
-    function lenth3(vector)
-        return math.sqrt(math.pow((vector.x),2)+math.pow((vector.y),2)+math.pow((vector.x),2))
-    end
+function lenth3(vector)
+    return math.sqrt(math.pow((vector.x),2)+math.pow((vector.y),2)+math.pow((vector.x),2))
+end
 
-    function lenth2(vector)
-        return math.sqrt(math.pow((vector.x),2)+math.pow((vector.y),2))
-    end
+function lenth2(vector)
+    return math.sqrt(math.pow((vector.x),2)+math.pow((vector.y),2))
+end
 
-    function dotp(value)
-        if value ~= 0 then
-            return value/math.abs(value)
-        end
-        return 0
+function dotp(value)
+    if value ~= 0 then
+        return value/math.abs(value)
     end
+    return 0
+end
 
+-- Lerped properties by Manuel_#2867
+-- Uses lerp to smooth out animations automatically, so you dont have to. Example:
+-- local cube = new_lerped_property(model.path.to.cube)
+-- then you can just set rotation, position, etc, inside tick() and it will automatically be smooth
+
+do
+function lerp(a, b, x)
+    return a + (b - a) * x
+end
+
+function lerp_3d(a, b, x)
+    return {lerp(a[1],b[1],x),lerp(a[2],b[2],x),lerp(a[3],b[3],x)}
+end
+
+function new_lerped_property(part)
+    local ret = {}
+    ret.prev_pos = {0,0,0}
+    ret.curr_pos = {0,0,0}
+    ret.enab_pos = false
+    ret.prev_rot = {0,0,0}
+    ret.curr_rot = {0,0,0}
+    ret.enab_rot = false
+    ret.prev_sca = {1,1,1}
+    ret.curr_sca = {1,1,1}
+    ret.enab_sca = false
+    ret.prev_uv = {0,0,0}
+    ret.curr_uv = {0,0,0}
+    ret.enab_uv = false
+    ret.prev_col = {1,1,1}
+    ret.curr_col = {1,1,1}
+    ret.enab_col = false
+    ret.prev_opa = 1
+    ret.curr_opa = 1
+    ret.enab_opa = false
+    function tick()
+        ret.prev_pos = ret.curr_pos
+        ret.prev_rot = ret.curr_rot
+        ret.prev_sca = ret.curr_sca
+        ret.prev_uv = ret.curr_uv
+        ret.prev_col = ret.curr_col
+        ret.prev_opa = ret.curr_opa
+    end
+    function render(delta)
+        if ret.enab_pos then part.setPos(lerp_3d(ret.prev_pos,ret.curr_pos,delta)) end
+        if ret.enab_rot then part.setRot(lerp_3d(ret.prev_rot,ret.curr_rot,delta)) end
+        if ret.enab_sca then part.setScale(lerp_3d(ret.prev_sca,ret.curr_sca,delta)) end
+        if ret.enab_uv then part.setUV(lerp_3d(ret.prev_uv,ret.curr_uv,delta)) end
+        if ret.enab_col then part.setColor(lerp_3d(ret.prev_col,ret.curr_col,delta)) end
+        if ret.enab_opa then part.setOpacity(lerp(ret.prev_opa,ret.curr_opa,delta)) end
+    end
+    local function setPos(pos)
+        ret.enab_pos = true
+        ret.curr_pos = pos
+    end
+    local function setRot(rot)
+        ret.enab_rot = true
+        ret.curr_rot = rot
+    end
+    local function setScale(scale)
+        ret.enab_sca = true
+        ret.curr_sca = scale
+    end
+    local function setUV(uv)
+        ret.enab_uv = true
+        ret.curr_uv = uv
+    end
+    local function setColor(color)
+        ret.enab_col = true
+        ret.curr_col = color
+    end
+    local function setOpacity(opacity)
+        ret.enab_opa = true
+        ret.curr_opa = opacity
+    end
+    ret.setPos = setPos
+    ret.setRot = setRot
+    ret.setScale = setScale
+    ret.setUV = setUV
+    ret.setColor = setColor
+    ret.setOpacity = setOpacity
+    return ret
+end
+end
+
+-- Define body parts
+head = new_lerped_property(model.Body.MIMIC_HEAD_MAIN.offset)
+
+body = new_lerped_property(model.Body)
+
+leftarm = new_lerped_property(model.Body.LeftArm)
+rightarm = new_lerped_property(model.Body.RightArm)
+
+lefthand = new_lerped_property(model.Body.LeftArm.LeftHand)
+righthand = new_lerped_property(model.Body.RightArm.RightHand)
+
+leftleg = new_lerped_property(model.Body.LeftLeg)
+rightleg = new_lerped_property(model.Body.RightLeg)
+
+leftfoot = new_lerped_property(model.Body.LeftLeg.LeftFoot)
+rightfoot = new_lerped_property(model.Body.RightLeg.RightFoot)
 
 --========================================================--
 --GNs animated player by GNamimates
